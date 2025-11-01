efb94ef4d1b172307cfda7b6855f831f53e57d6f
ayushmurugkar11 <ayushmurugkar1234@gmail.com>
2025-11-01 22:55:57 +0530

    Define PORT variable with fallback value

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import server from "./app.js";
-import { PORT } from "./solution.js";
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   console.log(`server is running on ${process.env.PORT}`);
